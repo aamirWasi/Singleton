84b39e845286c6c59d92d5a8587c0df419369406
aamir <wasi3aamir@gmail.com>
2020-04-30 17:14:47 +0600

    OOAD class 11

---
+++ OOAD.Singleton/OOD.SingletonClass11/Motor.cs
@@ -0,0 +1,21 @@
+﻿namespace OOD.SingletonClass11
+{
+    public class Motor
+    {
+        private static Motor _motor;
+        public static int counter = 0;
+        private Motor()
+        {
+            counter++;
+        }
+        public static Motor GetMotor
+        {
+            get
+            {
+                if (_motor == null)
+                    return _motor = new Motor();
+                return _motor;
+            }
+        }
+    }
+}
---
+++ OOAD.Singleton/OOD.SingletonClass11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OOD.SingletonClass11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Motor.counter);
+            var motor1 = Motor.GetMotor;
+            var motor2 = Motor.GetMotor;
+            var motor3 = Motor.GetMotor;
+            Console.WriteLine(Motor.counter);
+        }
+    }
+}
