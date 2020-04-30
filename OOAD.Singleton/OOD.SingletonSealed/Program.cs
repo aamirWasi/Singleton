e66aa6227b6ea979a0d7dbad1afbf3524fc42977
aamir <wasi3aamir@gmail.com>
2020-04-30 13:55:26 +0600

    add sealed in singletonSealed

---
+++ OOAD.Singleton/OOD.SingletonSealed/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace OOD.SingletonSealed
+{
+    public sealed class SingletonSealed
+    {
+        private static int counter = 0;
+        private static SingletonSealed _instance;
+
+        private SingletonSealed()
+        {
+            counter++;
+            Console.WriteLine($"Count value = {counter}");
+        }
+        public static SingletonSealed GetInstance
+        {
+            get
+            {
+                if (_instance == null)
+                    return _instance = new SingletonSealed();
+                return _instance;
+            }
+        }
+        public void Write(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+        //public class DerivedSingleton : SingletonSealed
+        //{
+
+        //}
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fromTeacher = SingletonSealed.GetInstance;
+            fromTeacher.Write("From Teacher");
+            var fromStudent = SingletonSealed.GetInstance;
+            fromStudent.Write("From Student");
+            //SingletonSealed.DerivedSingleton derivedSingleton = new SingletonSealed.DerivedSingleton();
+            //derivedSingleton.Write("derived");
+
+        }
+    }
+}
