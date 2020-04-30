1be14058247839071d73fc86a78dd77a57684990
aamir <wasi3aamir@gmail.com>
2020-04-30 12:49:27 +0600

    Normal singleton with a write()

--- OOAD.Singleton/OOD.Singleton/Program.cs
+++ OOAD.Singleton/OOD.Singleton/Program.cs
@@ -2,11 +2,49 @@
 
 namespace OOD.Singleton
 {
+    public interface IConsoleLoggerCilent
+    {
+        void Write(string message);
+    }
+
+    public class ConsoleLoggerCilent : IConsoleLoggerCilent
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton _instance;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"Count value = {counter}");
+        }
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+                return _instance = new Singleton();
+            return _instance;
+        }
+        public void Write(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+       
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fromTeacher = Singleton.GetInstance();
+            fromTeacher.Write("From Teacher");
+            var fromStudent = Singleton.GetInstance();
+            fromStudent.Write("From Student");
         }
     }
 }
